0528fc3aede9c27a3e362dc1f18c67ec41471b89
Volleier <1206670314@qq.com>
2025-02-13 23:26:52 +0800

    Health

--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -92,7 +92,17 @@ private:
 	float TimeSinceLastMovementReplication;
 	float CalculateSpeed();
 
-public:
+	/*
+	 * ½ÇÉ«ÉúÃüÖµ
+	 */
+	UPROPERTY(EditAnywhere,Category = "Player States")
+	float MaxHealth = 100.f;
+	UPROPERTY(ReplicatedUsing = OnRep_Health, VisibleAnywhere, Category = "Player States")
+	float Health = 100.f;
+
+	UFUNCTION()
+	void OnRep_Health();
+public:	
 	void SetOverlappingWeapon(AWeapon* Weapon);
 	bool IsWeaponEquipped();
 	bool IsAiming();
