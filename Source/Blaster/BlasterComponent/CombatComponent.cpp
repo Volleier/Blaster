e648a0ddd003e276ae498e537729587b05c6b4b2
Volleier <1206670314@qq.com>
2025-09-12 15:36:39 +0800

    Hit Boxes

--- Source/Blaster/BlasterComponent/CombatComponent.cpp
+++ Source/Blaster/BlasterComponent/CombatComponent.cpp
@@ -481,9 +481,14 @@ bool UCombatComponent::CanFire()
 	if (EquippedWeapon == nullptr)
 		return false;
 	if (bLocallyReloading) 
-		return false;
-	if (!EquippedWeapon->IsEmpty() && bCanFire && CombatState == ECombatState::ECS_Reloading && EquippedWeapon->GetWeaponType() == EWeaponType::EWT_Shotgun)
-		return true;
+	{
+		// 允许在重新装填时开枪
+		if (!EquippedWeapon->IsEmpty() && bCanFire && CombatState == ECombatState::ECS_Reloading && EquippedWeapon->GetWeaponType() == EWeaponType::EWT_Shotgun)
+		{
+			bLocallyReloading = false;
+			return true;
+		}
+	}
 	return !EquippedWeapon->IsEmpty() && bCanFire && CombatState == ECombatState::ECS_Unoccupied;
 }
 
